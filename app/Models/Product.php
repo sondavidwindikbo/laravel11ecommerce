3178e4dd91fa53d2cb0e74514a7dc4484e792ff6
unknown <windikboson@gmail.com>
2025-09-18 11:33:54 +0900

    make admin product page v14

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,7 +14,8 @@ class Product extends Model
         return $this->belongsTo(Category::class,'category_id');
     }
 
-    public function brand(){
+    public function brand()
+    {
         return $this->belongsTo(Brand::class,'brand_id');
     }
 }
