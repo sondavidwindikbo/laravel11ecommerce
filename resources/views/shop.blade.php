7ba3c5693b3a0df45f677cd737dc4c278d6fe5df
unknown <windikboson@gmail.com>
2025-09-26 00:36:40 +0900

    create Products Filter By Category v25

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 @section('content')
+    <style>
+      .brand-list li, .category-list li{
+        line-height: 40px;
+      }
+
+      .brand-list li .chk-brand , .category-list li .chk-category{
+        width: 1rem;
+        height: 1rem;
+        color: #e4e4e4;
+        border: 0.125rem solid currentColor;
+        border-radius: 0;
+        margin-right: 0.75rem;
+      }
+    </style>
      <main class="pt-90">
     <section class="shop-main container d-flex pt-4 pt-xl-5">
       <div class="shop-sidebar side-sticky bg-body" id="shopFilter">
@@ -26,38 +40,19 @@
             </h5>
             <div id="accordion-filter-1" class="accordion-collapse collapse show border-0"
               aria-labelledby="accordion-heading-1" data-bs-parent="#categories-list">
-              <div class="accordion-body px-0 pb-0 pt-3">
+              <div class="accordion-body px-0 pb-0 pt-3 category-list">
                 <ul class="list list-inline mb-0">
-                  <li class="list-item">
-                    <a href="#" class="menu-link py-1">Dresses</a>
-                  </li>
-                  <li class="list-item">
-                    <a href="#" class="menu-link py-1">Shorts</a>
-                  </li>
-                  <li class="list-item">
-                    <a href="#" class="menu-link py-1">Sweatshirts</a>
-                  </li>
-                  <li class="list-item">
-                    <a href="#" class="menu-link py-1">Swimwear</a>
-                  </li>
-                  <li class="list-item">
-                    <a href="#" class="menu-link py-1">Jackets</a>
-                  </li>
-                  <li class="list-item">
-                    <a href="#" class="menu-link py-1">T-Shirts & Tops</a>
-                  </li>
-                  <li class="list-item">
-                    <a href="#" class="menu-link py-1">Jeans</a>
-                  </li>
-                  <li class="list-item">
-                    <a href="#" class="menu-link py-1">Trousers</a>
-                  </li>
-                  <li class="list-item">
-                    <a href="#" class="menu-link py-1">Men</a>
-                  </li>
-                  <li class="list-item">
-                    <a href="#" class="menu-link py-1">Jumpers & Cardigans</a>
-                  </li>
+                  @foreach ($categories as $category)         
+                    <li class="list-item">
+                      <span class="menu-link py-1">
+                        <input type="checkbox" class="chk-category" name="categories" value="{{$category->id}}"
+                        @if(in_array($category->id,explode(',',$f_categories))) checked="checked" @endif
+                        />
+                        {{$category->name}}
+                      </span>
+                      <span class="text-right float-end" >{{$category->products->count()}}</span>
+                    </li>
+                   @endforeach
                 </ul>
               </div>
             </div>
@@ -436,6 +431,7 @@
     <input type="hidden" name="size" id="size" value="{{$size}}" />
     <input type="hidden" name="order" id="order" value="{{$order}}" />
     <input type="hidden" name="brands" id="hdnBrands" />
+    <input type="hidden" name="categories" id="hdnCategories">
   </form>
 
 @endsection
@@ -464,7 +460,23 @@
             brands += "," + $(this).val();
           }
         });
-        $("#hdnBrands").val(brands);
+        $("#hdnCategories").val(brands);
+        $("#frmfilter").submit();
+      });
+
+
+       $("input[name='categories']").on("change",function(){
+        var categories = "";
+        $("input[name='categories']:checked").each(function(){
+          if(categories == "")
+          {
+            categories += $(this).val();
+          }
+          else{
+            categories += "," + $(this).val();
+          }
+        });
+        $("#hdnCategories").val(categories);
         $("#frmfilter").submit();
       });
 
