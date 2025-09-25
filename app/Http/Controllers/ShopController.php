65fa8ca969a3e13a2a61a556f5cc62cb4c5143f1
unknown <windikboson@gmail.com>
2025-09-25 21:58:05 +0900

    create Products Filter By Brand v24

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,7 @@ class ShopController extends Controller
         $o_column = "";
         $o_order = "";
         $order = $request->query('order') ? $request->query('order') : -1;
+        $f_brands = $request->query('brands');
         switch($order)
         {
             case 1:
@@ -36,8 +38,12 @@ class ShopController extends Controller
             $o_order = 'DESC';
         }
 
-        $products = Product::orderBy($o_column,$o_order)->paginate($size);
-        return view('shop',compact('products','size','order'));
+        $brands = Brand::orderBy('name','ASC')->get();
+        $products = Product::where(function($query) use($f_brands){
+            $query->whereIn('brand_id',explode(',',$f_brands))->orWhereRaw("'".$f_brands."'=''");
+        })
+                        ->orderBy($o_column,$o_order)->paginate($size);
+        return view('shop',compact('products','size','order','brands','f_brands'));
     }
 
     public function product_details($product_slug)
