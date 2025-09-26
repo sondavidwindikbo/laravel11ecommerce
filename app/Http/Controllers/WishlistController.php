2f7e5f237cb25f34b0c47088cfc476d85bef5e74
unknown <windikboson@gmail.com>
2025-09-26 13:48:41 +0900

    create Show All Wishlisted Products v28

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -7,6 +7,13 @@ use Surfsidemedia\Shoppingcart\Facades\Cart;
 
 class WishlistController extends Controller
 {
+
+    public function index()
+    {
+        $items = Cart::instance('wishlist')->content();
+        return view('wishlist',compact('items'));
+    }
+
     public function add_to_wishlist(Request $request)
     {
         Cart::instance('wishlist')->add($request->id,$request->name,$request->quantity,$request->price)->associate('App\Models\Product');
--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,13 @@ Route::get('/shop/{product_slug}',[ShopController::class,'product_details'])->na
 
 Route::get('/cart',[CartController::class,'index'])->name('cart.index');
 Route::post('/cart/add',[CartController::class,'add_to_cart'])->name('cart.add');
-Route::put('cart/increase-quantity/{rowId}',[CartController::class, 'increase_cart_quantity'])->name('cart.qty.increase');
-Route::put('cart/decrease-quantity/{rowId}',[CartController::class, 'decrease_cart_quantity'])->name('cart.qty.decrease');
+Route::put('/cart/increase-quantity/{rowId}',[CartController::class, 'increase_cart_quantity'])->name('cart.qty.increase');
+Route::put('/cart/decrease-quantity/{rowId}',[CartController::class, 'decrease_cart_quantity'])->name('cart.qty.decrease');
 Route::delete('/cart/remove/{rowId}', [CartController::class,'remove_item'])->name('cart.item.remove');
 Route::delete('/cart/clear', [CartController::class,'empty_cart'])->name('cart.empty');
 
 Route::post('/wishlist/add',[WishlistController::class,'add_to_wishlist'])->name('wishlist.add');
+Route::get('/wishlist', [WishlistController::class,'index'])->name('wishlist.index');
 
 
 Route::middleware(['auth'])->group(function(){
