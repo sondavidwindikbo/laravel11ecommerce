4e60ca6251578a841d17516186f2d11a6006fd08
unknown <windikboson@gmail.com>
2025-09-26 11:24:34 +0900

    create Project- Add Product To Wishlist v27

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WishlistController;
 use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,8 @@ Route::put('cart/increase-quantity/{rowId}',[CartController::class, 'increase_ca
 Route::put('cart/decrease-quantity/{rowId}',[CartController::class, 'decrease_cart_quantity'])->name('cart.qty.decrease');
 Route::delete('/cart/remove/{rowId}', [CartController::class,'remove_item'])->name('cart.item.remove');
 Route::delete('/cart/clear', [CartController::class,'empty_cart'])->name('cart.empty');
+
+Route::post('/wishlist/add',[WishlistController::class,'add_to_wishlist'])->name('wishlist.add');
 
 
 Route::middleware(['auth'])->group(function(){
