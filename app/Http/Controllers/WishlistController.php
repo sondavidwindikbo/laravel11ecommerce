044207f8b7e80da2068a35af384510e9e783fc09
unknown <windikboson@gmail.com>
2025-09-26 15:34:28 +0900

    create Remove Product from Wishlist

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -19,4 +19,16 @@ class WishlistController extends Controller
         Cart::instance('wishlist')->add($request->id,$request->name,$request->quantity,$request->price)->associate('App\Models\Product');
         return redirect()->back();
     }
+
+    public function remove_item($rowId){
+        Cart::instance('wishlist')->remove($rowId);
+        return redirect()->back();
+    }
+
+    public function empty_wishlist()
+    {
+        Cart::instance('wishlist')->destroy();
+        return redirect()->back();
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,9 @@ Route::delete('/cart/clear', [CartController::class,'empty_cart'])->name('cart.e
 
 Route::post('/wishlist/add',[WishlistController::class,'add_to_wishlist'])->name('wishlist.add');
 Route::get('/wishlist', [WishlistController::class,'index'])->name('wishlist.index');
+Route::delete('/wishlist/item/remove/{rowId}',[WishlistController::class,'remove_item'])->name('wishlist.item.remove');
+Route::delete('/wishlist/clear',[WishlistController::class,'empty_wishlist'])->name('wishlist.items.clear');
+
 
 
 Route::middleware(['auth'])->group(function(){
