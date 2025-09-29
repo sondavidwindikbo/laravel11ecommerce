082bae08400c29f726bd0df63f28c71fae17ca77
unknown <windikboson@gmail.com>
2025-09-29 21:42:50 +0900

    Move Product From Wishlist to Cart v30

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -31,4 +31,11 @@ class WishlistController extends Controller
         return redirect()->back();
     }
 
+    public function move_to_cart($rowId){
+        $item = Cart::instance('wishlist')->get($rowId);
+        Cart::instance('wishlist')->remove($rowId);
+        Cart::instance('cart')->add($item->id,$item->name,$item->qty,$item->price)->associate('App\Models\Product');
+        return redirect()->back();
+    }
+
 }
