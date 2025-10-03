9017b0ef001d4c22ca799e94d50fbf04a884c850
unknown <windikboson@gmail.com>
2025-10-03 14:28:09 +0900

    create  Checkout With Cash On Delivery v39

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -14,7 +14,7 @@ class Order extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function OrderItem()
+    public function OrderItems()
     {
         return $this->hasMany(OrderItem::class);
     }
