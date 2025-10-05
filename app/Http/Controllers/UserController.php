b5fc98dedde6c9fdfb693b243f7f6f6ef07927e4
unknown <windikboson@gmail.com>
2025-10-05 14:33:19 +0900

    create  User Show Order and Order Details 42

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('user.index');
+    }
+
+    public function orders()
+    {
+        $orders = Order::where('user_id', Auth::user()->id)->orderBy('created_at', 'DESC')->paginate(10);
+        return view('user.orders', compact('orders'));
+    }
+
+    public function order_details($order_id)
+    {
+        $order = Order::where('user_id', Auth::user()->id)->where('id', $order_id)->first();
+        if ($order) {
+            $orderItems = OrderItem::where('order_id', $order->id)->paginate(12);
+            $transaction = Transaction::where('order_id', $order->id)->first();
+            return view('user.order-details', compact('order', 'orderItems', 'transaction'));
+        } else {
+            return redirect()->route('login');
+        }
     }
 }
