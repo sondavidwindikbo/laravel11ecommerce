c95ea6bb208b8955475219a9e839814e1fd5d5be
unknown <windikboson@gmail.com>
2025-10-12 00:49:05 +0900

    create Show Featured Products On Home Page v49

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\Slide;
 use Illuminate\Http\Request;
 
@@ -14,6 +15,8 @@ class HomeController extends Controller
     {
         $slides = Slide::where('status', 1)->get()->take(3);
         $categories = Category::orderBy('name')->get();
-        return view('index', compact('slides', 'categories'));
+        $sproducts = Product::whereNotNull('sale_price')->where('sale_price', '<>', '')->inRandomOrder()->get()->take(8);
+        $fproducts = Product::where('featured', 1)->get()->take(8);
+        return view('index', compact('slides', 'categories', 'sproducts', 'fproducts'));
     }
 }
